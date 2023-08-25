616e36d37aae4ca6af67e4186fa2bdd00b7500d4
rafael.lumang <lumang.rafael@gmail.com>
2023-08-24 23:02:04 -0700

    Moved middleware files + authentication successful

--- routes.js
+++ routes.js
@@ -1,30 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const { User, Course } = require('./models')
-
-// async Handler to run functions through error try catch block
-
-function asyncHandler(cb) {
-    return async (req, res, next) => {
-        try {
-            await cb(req, res, next);
-        } catch (err) {
-            next(err);
-        }
-    }
-}
+const { asyncHandler } = require('./middleware/async-handler');
+const { error400Handler } = require('./middleware/error-400-handler');
+const { authenticateUser } = require('./middleware/auth-user');
 
 
 // Return all properties & values for currently authenticated User + 200 status 
-router.get('/users', asyncHandler(async (req, res) => {
+router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
+    const user = req.currentUser;
     const users = await User.findAll();
-    res.json(users).status(200);
+    res.status(200).json(users).json({ name: user.name, username: user.username});
 }));
 
-// Create a new user, set Location header to "/" +  201 status
 router.post('/users', asyncHandler(async (req, res) => {
-    await User.create(req.body);
-    res.location('/').status(201).end();
+    try {
+        await User.create(req.body);
+        res.location('/').status(201).end();
+    } catch (error) {
+        error400Handler(error, res);
+    }
 }));
 
 // Return all courses including User associated + 200 status
@@ -42,33 +37,49 @@ router.get('/courses/:id', asyncHandler(async (req, res) => {
 /* Create a new course, set Location header to the URI of created course 
 + 201 status + no content
 */
-router.post('/courses', asyncHandler(async (req, res) => {
-    let course;
-    if (req.body.title && req.body.description && req.body.userId) {
-        course = await Course.create({
-            title: req.body.title,
-            description: req.body.description,
-            userId: req.body.userId
-        });
+router.post('/courses', authenticateUser, asyncHandler(async (req, res) => {
+
+    try {
+        let course;
+        if (req.body.title && req.body.description && req.body.userId) {
+            course = await Course.create({
+                title: req.body.title,
+                description: req.body.description,
+                userId: req.body.userId
+            });
+            res.location(`localhost:5000/api/courses/${course.id}`).status(201).end();
+        } else {
+            res.status(400).json({ message: 'Incomplete course data for creation' });
+        }
+
+    } catch (error) {
+        error400Handler(error, res);
     }
-    res.location(`localhost:5000/api/courses/${course.id}`).status(201).end();
+
 }));
 
 // Update the corresponding course + 204 status + no content
-router.put('/courses/:id', asyncHandler(async (req, res) => {
-    const course = await Course.findByPk(req.params.id);
-    if (course) {
-        await course.update({
-            title: req.body.title,
-            description: req.body.description,
-            userId: req.body.userId
-        })
+router.put('/courses/:id', authenticateUser, asyncHandler(async (req, res) => {
+    try {
+        const course = await Course.findByPk(req.params.id);
+            if (course && req.body.title && req.body.description) {
+                await course.update({
+                    title: req.body.title,
+                    description: req.body.description,
+                    userId: req.body.userId
+                })
+            res.status(204).end();
+        } else {
+            res.status(400).json({ message: 'Incomplete course data for updating' });
+        }
+    } catch (error) {
+        error400Handler(error, res);
     }
-    res.status(204).end();
+
 }));
 
 // Delete the corresponding course + 204 status + no content
-router.delete('/courses/:id', asyncHandler(async (req, res) => {
+router.delete('/courses/:id', authenticateUser, asyncHandler(async (req, res) => {
     const course = await Course.findByPk(req.params.id);
     await course.destroy();
     res.status(204).end();
